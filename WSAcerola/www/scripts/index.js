1ef0fcbf33b030ca11fe8895ce3c2b2b409554e1
高見知英 <chie@onpu-tamago.net>
2015-08-28 14:53:09 +0900

    初回コミット

---
+++ WSAcerola/www/scripts/index.js
@@ -0,0 +1,20 @@
+﻿// 空白のテンプレートの概要については、次のドキュメントを参照してください:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// ページ上のコードをデバッグするには、Ripple で読み込むか、Android デバイス/エミュレーターで読み込みます。アプリを起動し、ブレークポイントを設定します。
+// 次に、JavaScript コンソールで "window.location.reload()" を実行します。
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+      document.addEventListener('pause', onPause.bind(this), false);
+      document.addEventListener('resume', onResume.bind(this), false);
+    };
+
+    function onPause() {
+    };
+
+    function onResume() {
+    };
+} )();
